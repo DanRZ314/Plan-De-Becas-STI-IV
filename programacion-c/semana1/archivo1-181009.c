7ff85a51b7401fca25a1acc09a5f9c48b9ca0c33
Daniela Romero <dan.arz04@gmail.com>
2018-10-09 13:58:33 -0700

    Primer programa

---
+++ programacion-c/semana1/archivo1-181009.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+int Numero;
+Numero = 1;
+
+printf("Hola Dan\n");
+printf("Hola Dan\t");
+printf("\nMi numero es %d", Numero);
+Numero = 3;
+printf("\nMi numero es %d", Numero);
+
+}
